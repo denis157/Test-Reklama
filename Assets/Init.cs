80ca666f14a2ad9fc855cc41f6c32a1bea8cb568
denis157 <52367106+denis157@users.noreply.github.com>
2021-01-22 11:59:30 +0300

    1
    без изменений

---
+++ Assets/Init.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleMobileAds.Api;
+public class Init : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        MobileAds.Initialize(initStatus => { });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/MobAdsBanner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using GoogleMobileAds.Api;
+
+public class MobAdsBanner : MonoBehaviour
+{
+    private BannerView bannerView;
+
+#if UNITY_ANDROID
+    private const string bannerUnitId = "ca-app-pub-3940256099942544/6300978111"; //тестовый айди
+#elif UNITY_IPHONE
+    private const string bannerUnitId = "";
+#else
+    private const string bannerUnitId = "unexpected_platform";
+#endif
+    void OnEnable()
+    {
+        bannerView = new BannerView(bannerUnitId, AdSize.Banner, AdPosition.Bottom);
+        AdRequest adRequest = new AdRequest.Builder().Build();
+        bannerView.LoadAd(adRequest);
+        StartCoroutine(ShowBanner());
+    }
+
+    IEnumerator ShowBanner()
+    {
+        yield return new WaitForSecondsRealtime(3.0f);
+        bannerView.Show();
+    }
+}
